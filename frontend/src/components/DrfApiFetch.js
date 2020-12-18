28ea99fd5769b3a9df7881190582b7445fb90986
haruyasu <harukun2002@gmail.com>
2020-12-18 13:53:51 +0900

    axios delete

--- frontend/src/components/DrfApiFetch.js
+++ frontend/src/components/DrfApiFetch.js
@@ -24,6 +24,15 @@ const DrfApiFetch = () => {
         .then(res => {setSelectedTask(res.data)})
     }
 
+    const deleteTask = () => {
+        axios.delete(`http://127.0.0.1:8000/api/tasks/${id}/`, {
+            headers: {
+                'Authorization': 'Token 7150f3fa640cc835e149b6c941ca3e6fb1abe281'
+            }
+        })
+        .then(res => console.log(res))
+    }
+
     return (
         <div>
             <ul>
@@ -35,6 +44,7 @@ const DrfApiFetch = () => {
             <input type='text' value={id} onChange={evt => {setId(evt.target.value)}} />
             <br />
             <button type='button' onClick={() => getTask()}>Get task</button>
+            <button type='button' onClick={() => deleteTask()}>Delete task</button>
             <h3>{selectedTask.title} {selectedTask.id}</h3>
         </div>
     )
